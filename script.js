1f3fd2aa9598a6302f48a4faa2f6965e9ad1a5a4
Buriak-byte <oleksii.buriak12@gmail.com>
2026-06-11 13:13:34 +0300

    ass

---
+++ script.js
@@ -0,0 +1,5 @@
+const user = document.querySelector(".user")
+let aLichilnyk = getComputedStyle(user).left
+let dLichilnyk = getComputedStyle(user).right
+console.log(aLichilnyk)
+console.log(dLichilnyk)
