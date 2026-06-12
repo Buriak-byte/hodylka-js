7d8210f603a0c780016013f68eb21acbe8fa16c5
Buriak-byte <oleksii.buriak12@gmail.com>
2026-06-12 10:52:11 +0300

    іфффффффф

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const user = document.querySelector(".user")
-let aLichilnyk = getComputedStyle(user).left
-let dLichilnyk = getComputedStyle(user).right
-console.log(aLichilnyk)
-console.log(dLichilnyk)
+let aLichilnyk = getComputedStyle(user).right
+let dLichilnyk = getComputedStyle(user).left
+let sLichilnyk = getComputedStyle(user).bottom
+let wLichilnyk = getComputedStyle(user).top
+document.addEventListener("keydown", (aEvent) => {
+    console.log(aEvent)
+if(aEvent.code === "KeyA"){
+    aLichilnyk -= 10
+}
+})
+document.addEventListener("keydown", (dEvent) => {
+  console.log(dEvent);
+  if (dEvent.code === "KeyD") {
+    dLichilnyk -= 10;
+  }
+});
+document.addEventListener("keydown", (sEvent) => {
+  console.log(sEvent);
+  if (sEvent.code === "KeyS") {
+    sLichilnyk -= 10;
+  }
+});
+document.addEventListener("keydown", (wEvent) => {
+  console.log(wEvent);
+  if (wEvent.code === "KeyW") {
+    wLichilnyk -= 10;
+  }
+});
